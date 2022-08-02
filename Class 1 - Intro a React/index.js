93650442782d9c7b89aad5e28ae4ca02f2120a53
gaby morgi <gabymorgi@gmail.com>
2022-08-02 19:44:30 -0300

    first class

---
+++ Class 1 - Intro a React/index.js
@@ -0,0 +1,30 @@
+function Title(props) {
+  return React.createElement('h1', null, props.children)
+}
+
+function Lista(props) {
+  console.log(props)
+  return React.createElement(
+    'ul',
+    null,
+    props.elementos.map((element, i) =>
+      React.createElement('li', { key: i, id: element }, element)))
+}
+
+function getElements() {
+  const games = getGameNames()
+  //console.log(games)
+  return [
+    React.createElement(Title, { key: "titulo-1" }, 'Juegos iconicos'),
+    React.createElement(Lista, { key: "lista-1", elementos: ['Mario', 'Sonic', 'Zelda'] }),
+    React.createElement(Title, { key: "titulo-2" }, 'Todos los juegos'),
+    React.createElement(Lista, { key: "lista-2", elementos: games }),
+  ]
+}
+
+window.addEventListener('load', function () {
+  ReactDOM.render(
+    getElements(),
+    document.getElementById('root')
+  )
+})
