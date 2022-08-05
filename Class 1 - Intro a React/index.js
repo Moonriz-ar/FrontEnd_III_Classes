13471d6ca31fdc50034be6380c877644420b9c27
gaby morgi <gabymorgi@gmail.com>
2022-08-05 19:45:37 -0300

    third class

--- Class 1 - Intro a React/index.js
+++ Class 1 - Intro a React/index.js
@@ -8,21 +8,26 @@ function Lista(props) {
     'ul',
     null,
     props.elementos.map((element, i) =>
-      React.createElement('li', { key: i, id: element }, element)))
+      React.createElement('li', { key: i, id: element }, element)
+    )
+  )
 }
 
 function getElements() {
   const games = getGameNames()
   //console.log(games)
   return [
-    React.createElement(Title, { key: "titulo-1" }, 'Juegos iconicos'),
-    React.createElement(Lista, { key: "lista-1", elementos: ['Mario', 'Sonic', 'Zelda'] }),
-    React.createElement(Title, { key: "titulo-2" }, 'Todos los juegos'),
-    React.createElement(Lista, { key: "lista-2", elementos: games }),
+    React.createElement(Title, { key: 'titulo-1' }, 'Juegos iconicos'),
+    React.createElement(Lista, {
+      key: 'lista-1',
+      elementos: ['Mario', 'Sonic', 'Zelda'],
+    }),
+    React.createElement(Title, { key: 'titulo-2' }, 'Todos los juegos'),
+    React.createElement(Lista, { key: 'lista-2', elementos: games }),
   ]
 }
 
 window.addEventListener('load', function () {
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(getElements());
+  const root = ReactDOM.createRoot(document.getElementById('root'))
+  root.render(getElements())
 })
